5934494cb6f8cd3a4ff9e1971e6882002417907d
Nicklas <nirada@live.dk>
2019-02-25 16:27:20 +0100

    serialize

--- src/main/java/dal/UserDAODisk.java
+++ src/main/java/dal/UserDAODisk.java
@@ -1,13 +1,20 @@
 package dal;
 
 import dto.UserDTO;
-
+import java.io.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAODisk implements IUserDAO {
-    //TODO: implement this shit
 
-    public UserDAODisk() {
+    List<UserDTO> user_list = new ArrayList<>();
+
+
+    public UserDAODisk() throws IOException {
+    }
+
+    public void open_file(){
+
     }
 
     @Override
@@ -22,6 +29,23 @@ public class UserDAODisk implements IUserDAO {
 
     @Override
     public void createUser(UserDTO user) throws DALException {
+
+        user_list.add(new UserDTO());
+
+        try{
+
+            FileOutputStream fos = new FileOutputStream("src\\main\\resources\\disk.txt");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(user_list);
+
+            oos.close();
+            fos.close();
+
+
+        }catch (IOException ioe){
+            ioe.printStackTrace();
+        }
 
     }
 
