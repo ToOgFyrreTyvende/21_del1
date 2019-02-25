d408a948de3150827ac6860fc547b3b93dcbd146
Theodor Lysberg Michaelsen <s174882@student.dtu.dk>
2019-02-25 16:54:48 +0100

    Create & delete user

--- src/main/java/dal/UserDAOMemory.java
+++ src/main/java/dal/UserDAOMemory.java
@@ -16,26 +16,31 @@ public class UserDAOMemory implements IUserDAO {
 
     @Override
     public UserDTO getUser(int userId) throws DALException {
+
+
         return null;
     }
 
     @Override
     public List<UserDTO> getUserList() throws DALException {
+
         return null;
     }
 
     @Override
     public void createUser(UserDTO user) throws DALException {
+        users.add(user);
 
     }
 
     @Override
     public void updateUser(UserDTO user) throws DALException {
 
+
     }
 
     @Override
     public void deleteUser(int userId) throws DALException {
-
+        users.remove(userId);
     }
 }
