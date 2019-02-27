e4b607790e990136d48b544367edfcc6f042ef79
Anders Michaelsen <37267649+Xephyz@users.noreply.github.com>
2019-02-27 16:08:59 +0100

    Update TUI.java
    **Added**:
    - Menu option to quit program
    - Failsafe for wrong inputs to the menu
    - Small bits of testing code for some of the methods
    - Confirmation step for the user deletion choice
    
    **Fixed**:
    - Input problem where some user choices were skipped automatically
    *(Basically just used `.next()` instead of `.nextLine()`* :ok_hand:
    
    Co-Authored-By: lysberg <lysberg@users.noreply.github.com>
    Co-Authored-By: nicklasdancker <nicklasdancker@users.noreply.github.com>
    Co-Authored-By: Ahmad Sattar <thehabbos007@users.noreply.github.com>
    Co-Authored-By: flaremew <flaremew@users.noreply.github.com>

--- src/main/java/ui/TUI.java
+++ src/main/java/ui/TUI.java
@@ -1,11 +1,14 @@
 package ui;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TUI {
     // TODO Remove comment when functionality layer is made :)
     //private IFunctionality func;
     private Scanner scan;
+    private boolean keepGoing = true;
+    //private boolean validUserChoice = true;
 
     public TUI(/*IFunctionality func*/){
         // TODO Remove comment when func layer is made :)
@@ -13,41 +16,58 @@ public class TUI {
     }
 
     public void showMenu(){
-        while (true){
+        while (keepGoing){
             scan = new Scanner(System.in);
+            int userChoice;
+
             System.out.println("---------------");
             System.out.println("1. Opret bruger");
             System.out.println("2. Vis brugere");
             System.out.println("3. Opdater bruger");
             System.out.println("4. Slet brugere");
+            System.out.println("0. Afslut program");
             System.out.println("---------------");
-            System.out.println("Indtast valg:");
+            System.out.println();
+            System.out.println("Indtast valg: ");
 
-            int userChoice = scan.nextInt();
-            System.out.println("\n");
-            scan.close();
+            try {
+                userChoice = scan.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Indtast venligst et tal!\n");
+                showMenu();
+                break;
+            }
+
+            System.out.println();
 
             switch (userChoice){
                 case 1:
-                    scan = new Scanner(System.in);
                     System.out.println("|-- Opret bruger -- |");
                     System.out.println("Indtast ønskede userID:");
                     int userID1 = scan.nextInt();
+
                     System.out.println("Indtast ønskede brugernavn:");
-                    String username1 = scan.nextLine();
-                    scan.close();
+                    String username1 = scan.next();
+
                     System.out.println("Indtast ønskede initialer:");
-                    scan = new Scanner(System.in);
-                    String ini1 = scan.nextLine();
-                    scan.close();
+                    String ini1 = scan.next();
+
                     System.out.println("Indtast cpr-nummer:");
-                    scan = new Scanner(System.in);
-                    String cpr1 = scan.nextLine();
-                    scan.close();
+                    String cpr1 = scan.next();
+
                     System.out.println("Indtast ønskede password:");
-                    scan = new Scanner(System.in);
-                    String password1 = scan.nextLine();
-                    scan.close();
+                    String password1 = scan.next();
+                    System.out.println();
+
+                    // -------------------- Testing -------------------- //
+                    System.out.println("|------- Testing -------|");
+                    System.out.println("Bruger indtastning var:");
+                    System.out.println("userID:\t\t" + userID1);
+                    System.out.println("username:\t" + username1);
+                    System.out.println("ini:\t\t" + ini1);
+                    System.out.println("cpr:\t\t" + cpr1);
+                    System.out.println("pass:\t\t" + password1);
+                    // ---------------------- End ---------------------- //
                     // TODO Run createUser method here when created
                     // TODO Confirm/report success/failure
                     break;
@@ -59,50 +79,59 @@ public class TUI {
                 case 3:
                     System.out.println("|-- Opdater brugere --|");
                     System.out.println("Indtast userID af ønskede bruger:");
-                    scan = new Scanner(System.in);
                     int userID3 = scan.nextInt();
-                    scan.close();
                     System.out.println("Indtast nye ønskede brugernavn:");
-                    scan = new Scanner(System.in);
-                    String username3 = scan.nextLine();
-                    scan.close();
+                    String username3 = scan.next();
                     System.out.println("Indtast nye ønskede initialer:");
-                    scan = new Scanner(System.in);
-                    String ini3 = scan.nextLine();
-                    scan.close();
+                    String ini3 = scan.next();
                     System.out.println("Indtast nye cpr-nummer:");
-                    scan = new Scanner(System.in);
-                    String cpr3 = scan.nextLine();
-                    scan.close();
+                    String cpr3 = scan.next();
                     System.out.println("Indtast nye ønskede password:");
-                    scan = new Scanner(System.in);
-                    String password3 = scan.nextLine();
-                    scan.close();
+                    String password3 = scan.next();
                     System.out.println("Indtast nye ønskede rolle:");
-                    scan = new Scanner(System.in);
-                    String role3 = scan.nextLine();
-                    scan.close();
+                    String role3 = scan.next();
+
+                    // -------------------- Testing -------------------- //
+                    System.out.println("|------- Testing -------|");
+                    System.out.println("userID:\t\t" + userID3);
+                    System.out.println("username:\t" + username3);
+                    System.out.println("ini:\t\t" + ini3);
+                    System.out.println("cpr:\t\t" + cpr3);
+                    System.out.println("pass:\t\t" + password3);
+                    System.out.println("role:\t\t" + role3);
+                    // ---------------------- End ---------------------- //
                     // TODO Update chosen user and print confirmation/error
                     break;
                 case 4:
                     System.out.println("|-- Slet bruger -- |");
                     System.out.println("Indtast userID af ønskede bruger");
-                    scan = new Scanner(System.in);
                     int userID4 = scan.nextInt();
-                    scan.close();
                     // TODO print chosen user for confirmation
                     System.out.println("Ønsker du at slette denne bruger? (Y/n)");
-                    scan = new Scanner(System.in);
-                    String yesNo = scan.nextLine();
-                    scan.close();
-                    if (yesNo == "Y"){
+                    String yesNo = scan.next();
+                    if ("Y".equals(yesNo)){
                         // TODO run deleteUser method when exists
                     } else {
                         System.out.println("Slet bruger aflyst!\n");
                     }
+                    // -------------------- Testing -------------------- //
+                    System.out.println("|------- Testing -------|");
+                    System.out.println("ID:\t\t" + userID4);
+                    System.out.println("Y/n:\t" + yesNo);
+                    // ---------------------- End ---------------------- //
+                    break;
+                case 0:
+                    System.out.println("|-- Afslut program --|");
+                    System.out.println("Bye bye!");
+                    keepGoing = false;
+                    break;
+                default:
+                    System.out.println("|-- Indtast venligst en valid mulighed fra listen! --|\n");
                     break;
             }
         }
+        // TODO Run something that ensures data doesn't die maybe?
+        // TODO (Not sure) Close any connections still open?
     }
 
 }
