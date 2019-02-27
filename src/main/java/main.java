bb3c538eda0cfd7f64f632b936f502a2f72b68a3
flare_000 <9BtNTiY4>
2019-02-27 20:47:44 +0100

    Implemented createUser, updateUser, deleteUser and possibly getUser. Wrote some stuff in main class for testing (will be deleted later)
    also added catch for NullPointException

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,5 +1,25 @@
+import data.dal.IUserDAO;
+import data.dal.UserDAOMySQL;
+import data.dto.UserDTO;
+
 public class main {
     public static void main(String[] args) {
         utils.SQLGenerator.generateTable();
+        IUserDAO sqlstuff = new UserDAOMySQL();
+        UserDTO user = new UserDTO();
+
+        user.setUserId(11);
+        user.setUserName("dummy");
+        user.setPassword("12345");
+        user.setIni("DD");
+        user.setCpr("123456-7890");
+        user.addRole("Admin");
+
+        try {
+            sqlstuff.createUser(user);
+            sqlstuff.getUser(11);
+        } catch (IUserDAO.DALException e) {
+            e.printStackTrace();
+        }
     }
 }
